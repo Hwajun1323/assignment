20058e9f81104808f76a0710e3b904558f4a774d
Hwajun <whajun1323@gmail.com>
2023-02-18 13:29:59 +0100

    1. Create Order - add check if email is existed

--- src/main/java/com/example/assignment/controller/OrderController.java
+++ src/main/java/com/example/assignment/controller/OrderController.java
@@ -3,22 +3,30 @@ package com.example.assignment.controller;
 import com.example.assignment.model.OrderDto;
 import com.example.assignment.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.jaxb.SpringDataJaxb;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("/order")
+    @PostMapping("")
     public ResponseEntity<String> createOrder(@RequestBody OrderDto orderDto) {
-        String orderId = orderService.createOrder(orderDto.getEmail(), orderDto.getProductId(), orderDto.getFirstName(), orderDto.getLastName());
-        return new ResponseEntity<>(orderId, HttpStatus.CREATED);
+        try {
+            String orderId = orderService.createOrder(orderDto);
+            return ResponseEntity.ok(orderId);
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/example/assignment/service/OrderService.java
+++ src/main/java/com/example/assignment/service/OrderService.java
@@ -1,23 +1,56 @@
 package com.example.assignment.service;
 
 import com.example.assignment.model.Order;
+import com.example.assignment.model.OrderDto;
+import com.example.assignment.model.UserListResponse;
 import com.example.assignment.repository.OrderRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
 
 @Service
 public class OrderService {
+    private ObjectMapper objectMapper;
+    private HttpClient httpClient;
 
+    public OrderService() {
+        this.objectMapper = new ObjectMapper();
+        this.httpClient = HttpClientBuilder.create().build();
+    }
     @Autowired
     private OrderRepository orderRepository;
 
-    public String createOrder(String email, String productId, String firstName, String lastName) {
+    public String createOrder(OrderDto orderDto) throws IOException {
+
+        // Check if the email exists in http://reqres.in/api/users
+        boolean emailExists = checkEmailExists(orderDto.getEmail());
+        if (!emailExists) {
+            throw new IllegalArgumentException("Email does not exist");
+        }
+
+        // Create a new order
         Order order = new Order();
-        order.setEmail(email);
-        order.setProductId(productId);
-        order.setFirstName(firstName);
-        order.setLastName(lastName);
+        order.setEmail(orderDto.getEmail());
+        order.setProductId(orderDto.getProductId());
+        order.setFirstName(orderDto.getFirstName());
+        order.setLastName(orderDto.getLastName());
+
         orderRepository.save(order);
         return order.getOrderId();
+    }
+
+    public boolean checkEmailExists(String email) throws IOException {
+        String url = "http://reqres.in/api/users";
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = httpClient.execute(request);
+
+        UserListResponse userListResponse = objectMapper.readValue(response.getEntity().getContent(), UserListResponse.class);
+        return userListResponse.getData().stream().anyMatch(user -> user.getEmail().equals(email));
     }
 }
