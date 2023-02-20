ad541c62315ad3b7feac37e5f05c26a3960a9877
Hwajun <whajun1323@gmail.com>
2023-02-20 14:48:14 +0100

    update dockerfile

--- src/main/java/com/example/assignment/controller/OrderController.java
+++ src/main/java/com/example/assignment/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+//http://localhost:8080/swagger-ui/#/
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
